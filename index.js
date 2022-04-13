9876c0b6ee748ee4c2ed54c1243cd335d7b2e4eb
Silpa <priyadarshini.silpa13@gmail.com>
2022-04-13 23:34:58 +0530

    API modified

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
 app.get('/timetable/student/:student_id', (req, res) => {
     
     let student_id = req.params.student_id;
